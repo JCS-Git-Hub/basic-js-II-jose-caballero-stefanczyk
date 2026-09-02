b80093771525ec7e89390cb436c6acae4bbfce2f
José Caballero Stefañczyk <j.caballero.stefan@gmail.com>
2026-09-02 11:03:42 +0200

    feat: solve conditional excercises in level 2

--- script.js
+++ script.js
@@ -10,43 +10,97 @@ console.log('Hola soy tu consola y juntas vamos a aprender Javascript')
 
 //Ejercicio 1: Escribe un condicional if/else que imprima en la consola 'Eres mayor de edad' cuando la constante age tenga el valor correspondiente, si no es así, debe imprimir 'No eres aún mayor de edad'
 
-const age = 18
+let age = 18
 //Escribe tu código aquí
-
+if (age >= 18) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("No eres aún mayor de edad");
+}
 
 //Ejercicio 2: Cambia el valor de age a 12 para mirar el resultado del ejercicio anterior.
-
+age = 12;
+if (age >= 18) {
+    console.log("Eres mayor de edad");
+} else {
+    console.log("No eres aún mayor de edad");
+}
 
 //Ejercicio 3: Escribe un condicional if/else que lea la constante pet y si es un perro, que imprima en la consola "Tengo un perro", si es un gato, que imprima en la consola "Tengo un gato", si es un pájaro, que imprima en la consola "Tengo un pájaro" y si no es ninguno de los 3 que imprima "No tengo una mascota convencional"
 
-const pet = "perro";
+let pet = "perro";
 //Escribe tu código aquí
-
+if (pet === "perro") {
+    console.log("Tengo un perro");
+} else if (pet === "gato") {
+    console.log("Tengo un gato");
+} else if (pet === "pájaro"){
+    console.log("Tengo un pájaro");
+} else {
+    console.log("No tengo una mascota convencional");
+}
 
 //Ejercicio 4: cambia el valor de pet a "serpiente" y mira el resultado en consola para comprobar que tu código funciona bien.
-
+pet = "serpiente"
+if (pet === "perro") {
+    console.log("Tengo un perro")
+} else if (pet === "gato") {
+    console.log("Tengo un gato")
+} else if (pet === "pájaro"){
+    console.log("Tengo un pájaro")
+} else {
+    console.log("No tengo una mascota convencional");
+}
 
 //Ejercicio 5: Haz el mismo ejercicio 3, pero con el condicional switch.
 
 //Escribe tu código aquí
+switch (pet) {
+    case "perro":
+        console.log("Tengo un perro");
+        break;
 
+    case "gato":
+        console.log("Tengo un gato");
+        break;
+
+    case "pájaro":
+        console.log("Tengo un pájaro");
+        break;
+
+    default:
+        console.log("No tengo una mascota convencional")
+}
 
 //Ejercicio 6: Usando un operador ternario, escribe un programa que lea la constante weather e imprima en consola si es soleado "Me vestiré con un vestido" y si no es así que imprima "Me vestiré con pantalón"
 
-const weather = "soleado"
+let weather = "soleado"
 
-const result = //Escribe tu código aquí;
+let result = weather === "soleado" ? "Me vestiré con un vestido" : "Me vestiré con un pantalón"; //Escribe tu código aquí;
 
 console.log(result)
 
 //Ejercicio 7: cambia el valor de weather a "frío" y mira el resultado en consola para comprobar que tu código funciona bien.
+weather = "frío";
+
+result = weather === "soleado" ? "Me vestiré con un vestido" : "Me vestiré con un pantalón";
+
+console.log(result);
 
 //Ejercicio 8: Escribe un programa que al darle la constante 'value' imprima en consola "Es un número" cuando el valor sea de tipo number, "Es un string" cuando el valor sea de tipo string o si no es ni uno ni otro que imprima "No es ni número ni string". Puedes hacerlo con cualquier tipo de condicional.
 
 const value = '1'
 
 //Escribe tu código aquí
+let valueComparion = value;
 
+if (typeof value === "number") {
+    console.log("Es un número");
+} else if (typeof value === "string") {
+    console.log("Es un string");
+} else {
+    console.log("No es ni un número ni string");
+}
 
 //BUCLES
 
